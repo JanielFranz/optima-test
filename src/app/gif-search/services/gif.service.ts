e90440bd78bf1b8cae4a2e45d690e55cbedc418c
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-20 13:13:35 -0500

    fix: httpclient added

--- src/app/gif-search/services/gif.service.ts
+++ src/app/gif-search/services/gif.service.ts
@@ -9,12 +9,12 @@ import {GifItem} from '../model/gif-item.entity';
 export class GifService {
 
   private httpOptions = { headers: new HttpHeaders ({ 'Content-Type' : 'application/json' }) };
-  private http: HttpClient = inject(HttpClient);
+  private http: HttpClient  = inject(HttpClient);
 
   private apiKey = 'MmfyhE1aaAOMjhhj9cKblhsH8QfnZGWL';
-  private baseUrl: string = `api.giphy.com/v1/gifs/search`
+  private baseUrl: string = 'https://api.giphy.com/v1/gifs/search'
 
-  public getGifsByWord(word: string) : Observable<GifItem[]> {
-    return this.http.get<GifItem[]>(`${this.baseUrl}?api_key=${this.apiKey}&q=${word}`, this.httpOptions);
+  public getGifsByWord(word: string) {
+    return this.http.get(`${this.baseUrl}?api_key=${this.apiKey}&q=${word}`, this.httpOptions);
   }
 }
