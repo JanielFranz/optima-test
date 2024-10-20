351f93123177f4179b6bafd26a9dd934c6376b67
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-20 13:13:53 -0500

    feat: app component with page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SearchGifComponent} from './gif-search/pages/search-gif/search-gif.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchGifComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
