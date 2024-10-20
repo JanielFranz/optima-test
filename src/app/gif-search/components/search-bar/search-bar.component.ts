4498c819dc9b1dcd2734f7abc1acccaf7f1ee0db
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-20 11:20:43 -0500

    feat: search bar component created

---
+++ src/app/gif-search/components/search-bar/search-bar.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+  //#region Properties
+  @Output() gifSearched = new EventEmitter<string>();
+
+  onWordEnter(word: string) {
+    this.gifSearched.emit(word)
+    console.log('word emmited: ', word);
+  }
+  //#endregion
+}
